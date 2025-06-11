cb45107dcf06ba3745e8dcefbb06b5035bdc3e54
batuhanaltog <batuhanaltog88@gmail.com>
2025-06-11 23:30:00 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+///////////////////////////////// 
+//FILES
+const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+
+const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+fs.writeFileSync("./txt/output.txt", textOut);
+
+// Non-blocking, asynchronous way
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+    if (err) return console.log("ERROR! 💥");
+    fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+        fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+            });
+        });
+    });
+});
+console.log("Will read file...");
+
+///////////////////////////////// 
+//SERVER
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8")
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+    const pathName = req.url;
+    if ( pathName === "/" || pathName === "/overview") {
+        res.end("This is the overview");
+    } else if (pathName === "/product") {
+        res.end("This is the product");
+    } else if (pathName === "/api") {
+        res.writeHead(200, {
+            "Content-type": "application/json"
+        });
+        res.end(data);
+    } else {
+        res.writeHead(404, {
+            "Content-type": "text/html",
+            "my-own-header": "hello-world"
+        });
+        res.end('<h1> This page could not be found!</h1>');
+    }
+})
+
+server.listen(8000, "127.0.0.1", () => {
+    console.log("Port Stands Up on port 8000 🔋🔋🔋");
+})
